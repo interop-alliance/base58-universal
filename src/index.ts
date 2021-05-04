786ec635e7416ddae38a63ece6a7f8f4d98066f6
Dmitri Zagidulin <dzagidulin@gmail.com>
2021-05-03 21:33:51 -0400

    Convert tests to TypeScript, add karma config.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 /*!
- * TypeScript implementation: Copyright (c) 2021 Interop Alliance and Dmitri Zagidulin.
+ * TypeScript conversion: Copyright (c) 2021 Interop Alliance and Dmitri Zagidulin.
  * Isomorphic JS implementation: Copyright (c) 2019-2020 Digital Bazaar, Inc. All rights reserved.
  */
 export { encode, decode } from './baseN'
--- test/api.spec.ts
+++ test/api.spec.ts
@@ -1,5 +1,6 @@
 /*!
- * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
+ * TypeScript conversion: Copyright (c) 2021 Interop Alliance and Dmitri Zagidulin.
+ * Mocha tests: Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
  */
 // @ts-ignore
 import chai from 'chai'
@@ -10,6 +11,10 @@ import { encode, decode } from '../src'
 /* eslint-disable max-len */
 // https://github.com/bitcoin/bitcoin/blob/master/src/test/data/base58_encode_decode.json
 const bitcoinVectors = [
+  /**
+   * Format:
+   * ['hex input string (to convert to Uint8Array)', 'encoded expected output']
+   */
   ['', ''],
   ['61', '2g'],
   ['626262', 'a3gV'],
@@ -33,7 +38,7 @@ const bitcoinVectors = [
     '000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9fa0a1a2a3a4a5a6a7a8a9aaabacadaeafb0b1b2b3b4b5b6b7b8b9babbbcbdbebfc0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedfe0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff',
     '1cWB5HCBdLjAuqGGReWE3R3CguuwSjw6RHn39s2yuDRTS5NsBgNiFpWgAnEx6VQi8csexkgYw3mdYrMHr8x9i7aEwP8kZ7vccXWqKDvGv3u1GxFKPuAkn8JCPPGDMf3vMMnbzm6Nh9zh1gcNsMvH3ZNLmP5fSG6DGbbi2tuwMWPthr4boWwCxf7ewSgNQeacyozhKDDQQ1qL5fQFUW52QKUZDZ5fw3KXNQJMcNTcaB723LchjeKun7MuGW5qyCBZYzA1KjofN1gYBV3NqyhQJ3Ns746GNuf9N2pQPmHz4xpnSrrfCvy6TVVz5d4PdrjeshsWQwpZsZGzvbdAdN8MKV5QsBDY'
   ]
-].map(([hexString, result]) => [_fromHexString(hexString), result])
+]
 /* eslint-enable max-len */
 
 // tests from README.md
@@ -41,86 +46,88 @@ const readmeVectors = [
   // from spec
   [[1, 2, 3, 4], '2VfUX'],
   [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], '4HUtbHhN2TkpR'],
-  [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], '4HUtbHhN2TkpR', 20],
-  [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], '4HUtbHhN\r\n2TkpR', 8],
   [[0x00, 0x00, 0x28, 0x7f, 0xb4, 0xcd], '11233QC4']
 ]
 
 // test from the spec
 const specVectors = [
-  ['Hello World!', '2NEpo7TZRRrLZSi2U'],
+  [new TextEncoder().encode('Hello World!'), '2NEpo7TZRRrLZSi2U'],
   [
-    'The quick brown fox jumps over the lazy dog.',
+    new TextEncoder().encode('The quick brown fox jumps over the lazy dog.'),
     'USm3fpXnKG5EUBx2ndxBDMPVciP5hGey2Jh4NDv6gmeo1LkMeiKrLJUUBk6Z'
   ],
   [_fromHexString('0000287fb4cd'), '11233QC4'],
   [
     // same as hex string above
-    [0x00, 0x00, 0x28, 0x7f, 0xb4, 0xcd],
+    new Uint8Array([0x00, 0x00, 0x28, 0x7f, 0xb4, 0xcd]),
     '11233QC4'
   ]
 ]
 
 // misc tests
 const miscVectors = [
-  [[], ''],
-  ['', ''],
-  ['a', '2g'],
-  ['ab', '8Qq'],
-  ['abc', 'ZiCa'],
-  [[0x00], '1'],
-  [[0x00, 0x00], '11']
-]
+  [new TextEncoder().encode(''), ''],
+  [new TextEncoder().encode('a'), '2g'],
+  [new TextEncoder().encode('ab'), '8Qq'],
+  [new TextEncoder().encode('abc'), 'ZiCa'],
 
-const testVectors = [
-  ...bitcoinVectors,
-  ...readmeVectors,
-  ...specVectors,
-  ...miscVectors
-].map(([data, result, maxline]) => [_toUint8Array(data), result, maxline])
+  [new Uint8Array([]), ''],
+  [new Uint8Array([0x00]), '1'],
+  [new Uint8Array([0x00, 0x00]), '11']
+]
 
 describe('base58-universal APIs', () => {
   describe('encode', () => {
     it('should properly encode', async () => {
-      const results = testVectors.map(([input]) => {
-        return [input, encode(input)]
-      })
-      results.forEach((r, i) => {
-        r[0].should.eql(testVectors[i][0])
-        r[1].should.equal(testVectors[i][1])
-      })
+      for (const [inputHex, expectedResult] of bitcoinVectors) {
+        const inputBytes = _fromHexString(inputHex)
+        const result = encode(inputBytes)
+        result.should.equal(expectedResult)
+      }
+
+      for (const [inputArray, expectedResult] of readmeVectors) {
+        const inputBytes = new Uint8Array(inputArray as number[])
+        const result = encode(inputBytes)
+        result.should.equal(expectedResult)
+      }
+
+      for (const [inputBytes, expectedResult] of [
+        ...specVectors,
+        ...miscVectors
+      ]) {
+        const result = encode(inputBytes as Uint8Array)
+        result.should.equal(expectedResult)
+      }
     })
   }) // end encode
 
   describe('decode', () => {
     it('should properly decode data', async () => {
-      const results = testVectors.map(([_, encoded]) => {
-        return [decode(encoded), encoded]
-      })
-      results.forEach((r, i) => {
-        r[0].should.eql(testVectors[i][0])
-        r[1].should.equal(testVectors[i][1])
-      })
+      for (const [expectedHex, inputString] of bitcoinVectors) {
+        const decodedBytes = _fromHexString(expectedHex)
+        const result = decode(inputString)
+        result.should.eql(decodedBytes)
+      }
+
+      for (const [expectedArray, inputString] of readmeVectors) {
+        const expectedBytes = new Uint8Array(expectedArray as number[])
+        const result = decode(inputString as string)
+        result.should.eql(expectedBytes)
+      }
+
+      for (const [expectedBytes, inputString] of [
+        ...specVectors,
+        ...miscVectors
+      ]) {
+        const result = decode(inputString as string)
+        result.should.eql(expectedBytes)
+      }
     })
   }) // end decode
 })
 
-function _toUint8Array(data) {
-  if (typeof data === 'string') {
-    // convert data to Uint8Array
-    return new TextEncoder().encode(data)
-  }
-  if (Array.isArray(data)) {
-    return new Uint8Array(data)
-  }
-  if (!(data instanceof Uint8Array)) {
-    throw new TypeError('"data" be a string, Array, or Uint8Array.')
-  }
-  return data
-}
-
 // https://stackoverflow.com/questions/38987784/how-to-convert-a-hexadecimal-string-to-uint8array-and-back-in-javascript
-function _fromHexString(hexString) {
+function _fromHexString(hexString: string): Uint8Array {
   if (hexString === '') {
     return new Uint8Array()
   }
